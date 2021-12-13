9de2d55b4ec6af07d326efd985ca64646895e315
gtluu <46392631+gtluu@users.noreply.github.com>
2021-12-13 12:20:17 -0800

    new figure s3 code with fittings

--- figures/figure_s3.R
+++ figures/figure_s3.R
@@ -1,14 +1,14 @@
 library(ggplot2)
 library(RColorBrewer)
 
-m901 <- read.csv('M901.csv')
+m901 <- read.csv('M901_updated.csv')
 colnames(m901)[1] <- 'time'
 m901$sample <- str_replace(m901$sample, '100pM', '100 pM Cystatin A')
 m901$sample <- str_replace(m901$sample, 'day0', 'Day 0')
 m901$sample <- str_replace(m901$sample, 'day14', 'Day 14')
 m901$sample <- str_replace(m901$sample, 'day28', 'Day 28')
 
-m902 <- read.csv('M902.csv')
+m902 <- read.csv('M902_updated.csv')
 colnames(m902)[1] <- 'time'
 m902$sample <- str_replace(m902$sample, '100pM', '100 pM Cystatin A')
 m902$sample <- str_replace(m902$sample, 'day0', 'Day 0')
@@ -16,7 +16,7 @@ m902$sample <- str_replace(m902$sample, 'day14', 'Day 14')
 m902$sample <- str_replace(m902$sample, 'day28', 'Day 28')
 m902$sample <- str_replace(m902$sample, 'day42', 'Day 42')
 
-m903 <- read.csv('M903.csv')
+m903 <- read.csv('M903_updated.csv')
 colnames(m903)[1] <- 'time'
 m903$sample <- str_replace(m903$sample, '100pM', '100 pM Cystatin A')
 m903$sample <- str_replace(m903$sample, 'day0', 'Day 0')
@@ -25,7 +25,7 @@ m903$sample <- str_replace(m903$sample, 'day28', 'Day 28')
 m903$sample <- str_replace(m903$sample, 'day42', 'Day 42')
 m903$sample <- str_replace(m903$sample, 'day56', 'Day 56')
 
-m904 <- read.csv('M904.csv')
+m904 <- read.csv('M904_updated.csv')
 colnames(m904)[1] <- 'time'
 m904$sample <- str_replace(m904$sample, '100pM', '100 pM Cystatin A')
 m904$sample <- str_replace(m904$sample, 'day0', 'Day 0')
@@ -33,7 +33,7 @@ m904$sample <- str_replace(m904$sample, 'day14', 'Day 14')
 m904$sample <- str_replace(m904$sample, 'day28', 'Day 28')
 m904$sample <- str_replace(m904$sample, 'day42', 'Day 42')
 
-m905 <- read.csv('M905.csv')
+m905 <- read.csv('M905_updated.csv')
 colnames(m905)[1] <- 'time'
 m905$sample <- str_replace(m905$sample, '100pM', '100 pM Cystatin A')
 m905$sample <- str_replace(m905$sample, 'day0', 'Day 0')
@@ -43,8 +43,9 @@ m905$sample <- str_replace(m905$sample, 'day42', 'Day 42')
 m905$sample <- str_replace(m905$sample, 'day56', 'Day 56')
 
 svg('nat_comm_figs3a.svg', width=16, height=9)
-ggplot(m901, aes(x=time, y=shift, colour=sample)) +
-  geom_line(size=1.5) +
+ggplot(m901, aes(x=time)) +
+  geom_line(aes(y=shift, colour=sample), size=1.5) +
+  geom_line(aes(y=fitting, colour=sample), size=1.5, linetype='dashed') +
   theme(panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         panel.background=element_blank(),
@@ -58,8 +59,9 @@ ggplot(m901, aes(x=time, y=shift, colour=sample)) +
 dev.off()
 
 svg('nat_comm_figs3b.svg', width=16, height=9)
-ggplot(m902, aes(x=time, y=shift, colour=sample)) +
-  geom_line(size=1.5) +
+ggplot(m902, aes(x=time)) +
+  geom_line(aes(y=shift, colour=sample), size=1.5) +
+  geom_line(aes(y=fitting, colour=sample), size=1.5, linetype='dashed') +
   theme(panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         panel.background=element_blank(),
@@ -73,8 +75,9 @@ ggplot(m902, aes(x=time, y=shift, colour=sample)) +
 dev.off()
 
 svg('nat_comm_figs3c.svg', width=16, height=9)
-ggplot(m903, aes(x=time, y=shift, colour=sample)) +
-  geom_line(size=1.5) +
+ggplot(m903, aes(x=time)) +
+  geom_line(aes(y=shift, colour=sample), size=1.5) +
+  geom_line(aes(y=fitting, colour=sample), size=1.5, linetype='dashed') +
   theme(panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         panel.background=element_blank(),
@@ -88,8 +91,9 @@ ggplot(m903, aes(x=time, y=shift, colour=sample)) +
 dev.off()
 
 svg('nat_comm_figs3d.svg', width=16, height=9)
-ggplot(m904, aes(x=time, y=shift, colour=sample)) +
-  geom_line(size=1.5) +
+ggplot(m904, aes(x=time)) +
+  geom_line(aes(y=shift, colour=sample), size=1.5) +
+  geom_line(aes(y=fitting, colour=sample), size=1.5, linetype='dashed') +
   theme(panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         panel.background=element_blank(),
@@ -103,8 +107,9 @@ ggplot(m904, aes(x=time, y=shift, colour=sample)) +
 dev.off()
 
 svg('nat_comm_figs3e.svg', width=16, height=9)
-ggplot(m905, aes(x=time, y=shift, colour=sample)) +
-  geom_line(size=1.5) +
+ggplot(m905, aes(x=time)) +
+  geom_line(aes(y=shift, colour=sample), size=1.5) +
+  geom_line(aes(y=fitting, colour=sample), size=1.5, linetype='dashed') +
   theme(panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         panel.background=element_blank(),
